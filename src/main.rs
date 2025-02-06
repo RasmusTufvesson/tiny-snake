b367e0e9d71785b726a1d074e92af84d4f191132
RasmusTufvesson <lars@bizzbook.com>
2025-02-06 23:21:13 +0100

    add input queue, better screen drawing, and consistent framerate

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ use std::{collections::VecDeque, time::{Duration, Instant}};
 use rand::Rng;
 use sdl2::{event::Event, keyboard::Scancode, pixels::Color, rect::Point};
 
-const FRAME_DELAY: u32 = 1_000_000_000u32 / 60;
+const FRAME_DELAY: u32 = 1_000_000_000 / 60;
 const MOVE_DELAY: f64 = 0.1;
+
+const PLAY_AREA: (u32, u32) = (30, 30);
+const WINDOW_SIZE: (u32, u32) = (PLAY_AREA.0 / 3, PLAY_AREA.1);
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-    let play_area = (30, 30);
-    let window_size = (play_area.0 / 3, play_area.1);
 
-    let window = video_subsystem.window("Tiny Snake", window_size.0, window_size.1)
+    let window = video_subsystem.window("Tiny Snake", WINDOW_SIZE.0, WINDOW_SIZE.1)
         .position_centered()
         .borderless()
         .input_grabbed()
@@ -25,10 +26,11 @@ fn main() {
     let mut rng = rand::rng();
 
     let mut segments = VecDeque::new();
-    segments.push_front((play_area.0 / 2, play_area.1 / 2));
+    segments.push_front((PLAY_AREA.0 / 2, PLAY_AREA.1 / 2));
     let mut direction = (0, 0);
     let mut move_timer = MOVE_DELAY;
-    let mut apple = (rng.random_range(0..play_area.0), rng.random_range(0..play_area.1));
+    let mut apple = (rng.random_range(0..PLAY_AREA.0), rng.random_range(0..PLAY_AREA.1));
+    let mut input_queue = VecDeque::new();
 
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
@@ -48,16 +50,16 @@ fn main() {
                 Event::KeyDown { scancode: Some(code), .. } => {
                     match code {
                         Scancode::W | Scancode::Up => {
-                            direction = (0, -1);
+                            input_queue.push_back((0, -1));
                         }
                         Scancode::S | Scancode::Down => {
-                            direction = (0, 1);
+                            input_queue.push_back((0, 1));
                         }
                         Scancode::A | Scancode::Left => {
-                            direction = (-1, 0);
+                            input_queue.push_back((-1, 0));
                         }
                         Scancode::D | Scancode::Right => {
-                            direction = (1, 0);
+                            input_queue.push_back((1, 0));
                         }
                         Scancode::Escape => {
                             return;
@@ -69,6 +71,10 @@ fn main() {
             }
         }
         
+        if let Some(dir) = input_queue.pop_front() {
+            direction = dir;
+        }
+
         move_timer -= delta_time;
         if move_timer <= 0.0 {
             move_timer = MOVE_DELAY;
@@ -76,31 +82,39 @@ fn main() {
             if x as u32 != apple.0 || y as u32 != apple.1 {
                 segments.pop_back().unwrap();
             } else {
-                apple = (rng.random_range(0..play_area.0), rng.random_range(0..play_area.1));
+                apple = (rng.random_range(0..PLAY_AREA.0), rng.random_range(0..PLAY_AREA.1));
             }
-            if x >= 0 && x < play_area.0 as i32 && y >= 0 && y < play_area.1 as i32 && !segments.contains(&(x as u32, y as u32)) {
+            if x >= 0 && x < PLAY_AREA.0 as i32 && y >= 0 && y < PLAY_AREA.1 as i32 && !segments.contains(&(x as u32, y as u32)) {
                 segments.push_front((x as u32, y as u32));
             } else {
                 segments.clear();
-                segments.push_front((play_area.0 / 2, play_area.1 / 2));
+                segments.push_front((PLAY_AREA.0 / 2, PLAY_AREA.1 / 2));
                 direction = (0,0);
-                apple = (rng.random_range(0..play_area.0), rng.random_range(0..play_area.1));
+                apple = (rng.random_range(0..PLAY_AREA.0), rng.random_range(0..PLAY_AREA.1));
             }
         }
 
-        for y in 0..window_size.1 {
-            for x in 0..window_size.0 {
-                let r = segments.contains(&(x*3,y)) || (apple.0 == x*3 && apple.1 == y);
-                let g = segments.contains(&(x*3+1,y)) || (apple.0 == x*3+1 && apple.1 == y);
-                let b = segments.contains(&(x*3+2,y)) || (apple.0 == x*3+2 && apple.1 == y);
-                if r || g || b {
-                    canvas.set_draw_color(Color::RGB(if r { 255 } else { 0 }, if g { 255 } else { 0 }, if b { 255 } else { 0 }));
+        let mut screen = [[[false; 3]; WINDOW_SIZE.0 as usize]; WINDOW_SIZE.1 as usize];
+
+        for segment in &segments {
+            screen[segment.1 as usize][segment.0 as usize / 3][segment.0 as usize % 3] = true;
+        }
+        screen[apple.1 as usize][apple.0 as usize / 3][apple.0 as usize % 3] = true;
+
+        for y in 0..WINDOW_SIZE.1 {
+            for x in 0..WINDOW_SIZE.0 {
+                let rgb = &screen[y as usize][x as usize];
+                if rgb[0] || rgb[1] || rgb[2] {
+                    canvas.set_draw_color(Color::RGB(if rgb[0] { 255 } else { 0 }, if rgb[1] { 255 } else { 0 }, if rgb[2] { 255 } else { 0 }));
                     canvas.draw_point(Point::new(x as i32, y as i32)).unwrap();
                 }
             }
         }
 
         canvas.present();
-        ::std::thread::sleep(Duration::new(0, FRAME_DELAY));
+        let diff = (last_frame - Instant::now()).as_nanos() as u32;
+        if diff < FRAME_DELAY {
+            ::std::thread::sleep(Duration::new(0, FRAME_DELAY - diff));
+        }
     }
 }
